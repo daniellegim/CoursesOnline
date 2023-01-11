f9bf2324df7c3ce5c4b646870a780daa3a050c5a
Danielle <211907670@idf.il>
2023-01-11 20:12:29 +0200

    add price and fix design

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,13 +1,20 @@
-import { Card, CardContent, CardHeader, Typography } from "@mui/material"
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material"
 
 const Course = (props) => {
     const course = props.course
-    
+
     return (
         <Card key={course.id}>
             <CardHeader title={course.name} />
             <CardContent>
-                <Typography>{course.description}</Typography>
+                <Grid container direction="row">
+                    <Grid item xs={11}>
+                        <Typography variant="h6">{course.description}</Typography>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <Typography variant="h6">{course.price}₪</Typography>
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,4 @@
+import { Grid } from "@mui/material"
 import Course from "./Course/Course"
 
 const Home = () => {
@@ -5,24 +6,32 @@ const Home = () => {
         {
             id: 1,
             name: "react",
-            description: "react course"
+            description: "react course",
+            price: "200"
         },
         {
             id: 2,
             name: "C++",
-            description: "C++ course"
+            description: "C++ course",
+            price: "150"
         },
         {
             id: 3,
             name: "Python",
-            description: "python course"
+            description: "python course",
+            price: "100"
         }
     ]
 
     return (
-        courses.map(course =>
-            <Course course={course} />
-        )
+        <Grid container justifyContent="center" spacing={2} sx={{ marginTop: "2em" }}>
+            {courses.map(course =>
+                <Grid item xs={8}>
+                    <Course course={course} />
+                </Grid>
+            )
+            }
+        </Grid>
     )
 }
 
