eb4f9d192a5a2ac16849f6c8d54e225e6a947958
גיא גלרט < @GG-PC>
2023-02-04 14:51:35 +0200

    Add More To Product Page

--- src/components/Course/CourseDetails.js
+++ src/components/Course/CourseDetails.js
@@ -1,56 +1,92 @@
-import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Rating, Typography } from "@mui/material"
+import {Grid, Container,Button, Card, CardActions, CardContent, CardHeader, Accordion,AccordionSummary,AccordionDetails, Rating, Typography } from "@mui/material"
 import { AddShoppingCart } from '@mui/icons-material'
+// import { Grid } from "@mui/material"
 import { useCartFunctions } from "../ShoppingCart/CartContext"
 import { useStyles } from "./style"
+import React from "react"
+import { ArrowForwardIosSharp } from "@mui/icons-material"
 
 const Course = (props) => {
     const classes = useStyles()
     const course = props.course
     const { addToCart } = useCartFunctions()
-
+    const [expanded, setExpanded] = React.useState("description");
     const handleAddToCart = (course) => {
         addToCart(course)
     }
+    const handleChange = (panel) => (event,newExpanded) =>{
+        setExpanded(newExpanded?panel:false);
+    }
 
     return (
-        <Card key={course._id} className={classes.card}>
+        <Container>
+            <Card key={course._id} className={classes.card} variant="outlined" >
 
-            <CardHeader
-                title={course.name}
-                action={
-                    <Typography variant="h6" sx={{ marginRight: "1em", marginTop: "0.5em" }}>{course.price}₪</Typography>
-                }
-                subheader={
-                    <>
-                        <Typography>{course.author}</Typography>
-                        <Typography >{course.category.name}</Typography>
-                    </>
-                }
-            />
-            <CardMedia 
-             component="img"
-             image={course.imgURL}
-             height="150"
-             alt={course.title}>
-
-            </CardMedia>
-            <CardContent>
-                <Typography variant="h6">{course.description}</Typography>
-            </CardContent>
-            <CardActions>
-                <Rating value={course.rating} precision={0.5} readOnly />
-                {!props.userCourses ?
-                    <Button
-                        variant="contained"
-                        endIcon={<AddShoppingCart />}
-                        onClick={() => handleAddToCart(course)}
-                        sx={{ marginLeft: "auto" }} >
-                        הוספה לסל
-                    </Button>
-                    : null
-                }
-            </CardActions>
-        </Card>
+                <CardHeader
+                    title={course.name}
+                    action={
+                        <Typography variant="h6" sx={{ marginRight: "1em", marginTop: "0.5em" }}>{course.price}₪</Typography>
+                    }
+                    subheader={
+                        <>
+                            <Typography>{course.author}</Typography>
+                            <Typography >{course.category.name}</Typography>
+                        </>
+                    }
+                />
+                <div
+                className={classes.centerCss} 
+                >
+                    <img src ={course.imgURL} alt={course.title}></img>
+                </div>
+                <CardContent>
+                    <Card   >
+                        <CardContent className={classes.bannerCard}>
+                        <Grid container spacing={2} sx={{ overflow: "auto", maxHeight: "73vh" }}>
+                        <Grid key="duration" item xs={4}>
+                            <Typography variant="h5">{course.duration}</Typography>
+                        </Grid>
+                        <Grid key="Level" item xs={4}>
+                            <Typography variant="h5">{course.level.name}</Typography>
+                        </Grid>
+                        <Grid key="Price" item xs={4}>
+                        <Typography variant="h5">{course.price}</Typography>
+                        </Grid>
+                        </Grid>
+                        </CardContent>
+                    </Card>
+                    <Accordion elevation={0} expanded={expanded === 'description'} onChange={handleChange('description')}>
+                        <AccordionSummary expandIcon={<ArrowForwardIosSharp/>}>
+                            <Typography variant="h5">Description</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography variant="h6">{course.description}</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion elevation={0} expanded={expanded === 'about'} onChange={handleChange('about')}>
+                        <AccordionSummary expandIcon={<ArrowForwardIosSharp/>}>
+                            <Typography variant="h5">About The Course</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography variant="h6">{course.about}</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </CardContent>
+                <CardActions>
+                    <Rating value={course.rating} precision={0.5} readOnly />
+                    {!props.userCourses ?
+                        <Button
+                            variant="contained"
+                            endIcon={<AddShoppingCart />}
+                            onClick={() => handleAddToCart(course)}
+                            sx={{ marginLeft: "auto" }} >
+                            הוספה לסל
+                        </Button>
+                        : null
+                    }
+                </CardActions>
+            </Card>
+        </Container>
     )
 }
 
--- src/components/Course/style.js
+++ src/components/Course/style.js
@@ -1,3 +1,4 @@
+import { grey } from '@mui/material/colors';
 import { createStyles, makeStyles } from '@mui/styles'
 
 export const useStyles = makeStyles((theme) =>
@@ -7,6 +8,17 @@ export const useStyles = makeStyles((theme) =>
             display: "flex",
             flexDirection: "column",
             justifyContent: "space-between"
+        },
+        bannerCard:{
+            backgroundColor:grey,
+            flexDirection: "column",
+            justifyContent: "space-between"
+        },
+        centerCss:{
+            display:"flex",
+            justifyItems:"center",
+            justifyContent:"center",
+            textAlign:"center"
         }
     })
 );
